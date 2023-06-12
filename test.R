63dd99737ae7c9cf880146d0459c08512b76a827
Binau02 <aubin.saunier@gmail.com>
2023-06-12 14:59:23 +0200

    age -14

--- test.R
+++ test.R
@@ -1,1 +1,16 @@
-print(substr("abcdef", 2, 4))
+df <- read.csv("stat_acc_V3.csv", sep = ";")
+
+# test <- is.na(df$place)
+# test <- sort(test)
+# print(c(rownames(df$Num_Acc[df$age == "NULL"])))
+
+test <- c()
+for (i in seq_len(nrow(df))) {
+  if (df$age[i] == "NULL") {
+    test <- append(test, i)
+  }
+}
+
+
+# print(rownames(df))
+# print(which(is.na(df$age)))
