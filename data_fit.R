f8ae5926f338370e1aa4c977903cdd24c34e9439
Binau02 <aubin.saunier@gmail.com>
2023-06-12 16:37:47 +0200

    plots

--- data_fit.R
+++ data_fit.R
@@ -4,11 +4,11 @@ gravity <- read.csv("niveaux_gravite.csv", sep = ";")
 dept <- read.csv("departements-france.csv")
 
 for (i in seq_len(nrow(weights))) {
-  df$descr_cat_veh[df$descr_cat_veh == weights[i, 1]] <- as.numeric(weights[i, 2])
+  df$weight[df$descr_cat_veh == weights[i, 1]] <- as.numeric(weights[i, 2])
 }
 
 for (i in seq_len(nrow(gravity))) {
-  df$descr_grav[df$descr_grav == gravity[i, 1]] <- as.numeric(gravity[i, 2])
+  df$gravity[df$descr_grav == gravity[i, 1]] <- as.numeric(gravity[i, 2])
 }
 
 for (i in seq_len(nrow(dept))) {
@@ -28,3 +28,23 @@ df <- df[-test, ]
 df$age <- as.numeric(df$age)
 df$age <- df$age - 14
 
+# plot by age
+hist(df$age, breaks = max(df$age)-min(df$age)+1, main = "number of accident by age", xlab = "age", ylab = "number of accident")
+
+# plot by athmospheric conditions
+barplot(table(df$descr_athmo), las = 2)
+barplot(table(df$descr_athmo)[-4], las = 2)
+
+# plot by surface
+barplot(table(df$descr_etat_surf), las = 2)
+barplot(table(df$descr_etat_surf)[-8], las = 2)
+
+# plot by gravity
+test <- table(df$descr_grav)
+barplot(as.table(c(test["Indemne"], test["Blessé léger"], test["Blessé hospitalisé"], test["Tué"])), las = 2)
+
+# plot by city
+# print(table(df[c("id_code_insee", "ville")]))
+test <- table(df["ville"])
+test <- test[order(test, decreasing = TRUE)]
+barplot(head(test, n = 30), las = 2)
--- test.R
+++ test.R
@@ -1,16 +1,1 @@
-df <- read.csv("stat_acc_V3.csv", sep = ";")
-
-# test <- is.na(df$place)
-# test <- sort(test)
-# print(c(rownames(df$Num_Acc[df$age == "NULL"])))
-
-test <- c()
-for (i in seq_len(nrow(df))) {
-  if (df$age[i] == "NULL") {
-    test <- append(test, i)
-  }
-}
-
-
-# print(rownames(df))
-# print(which(is.na(df$age)))
+print(as.Date("05/01/2009  11:45:00"))
