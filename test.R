e3895006b8c2b21a2d06fec8382261a5e4569c90
Binau02 <aubin.saunier@gmail.com>
2023-06-12 12:00:27 +0200

    replace string with matching weight

--- test.R
+++ test.R
@@ -1,3 +1,5 @@
 # test <- read.csv('/Users/aubin/Documents/ecole/isen/CIR3/big data/projet_big_data/stat_acc_V3.csv', sep=';')
 test <- read.csv('stat_acc_V3.csv', sep=';')
-print(test)
+test$descr_cat_veh[test$descr_cat_veh == 'Autobus'] <- 20000
+# print(test$descr_cat_veh)
+# print(test)
