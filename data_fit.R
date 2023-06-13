9d1c6c53dbe8cb2201f20437ad9e218b588fa8d2
Binau02 <aubin.saunier@gmail.com>
2023-06-13 15:05:09 +0200

    oui

--- data_fit.R
+++ data_fit.R
@@ -71,7 +71,9 @@ dev.off()
 # plot by city
 test <- table(df["ville"])
 test <- test[order(test, decreasing = TRUE)]
+png(file = "./export/nb_acc_par_ville_top_30.png")
 barplot(head(test, n = 30), las = 2)
+dev.off()
 
 #map
 extraWD <- "."
@@ -104,10 +106,10 @@ for (i in seq_len(nrow(df))) {
   n <- as.numeric(substr(df$id_code_insee[i], 1, 2))
   if (is.na(n)) {
     if (substr(df$id_code_insee[i], 1, 2) == "2A") {
-      n = 96
+      n <- 96
     }
     else {
-      n = 97
+      n <- 97
     }
   }
   if (n >= 1 && n <= 96) {
@@ -126,15 +128,15 @@ for (i in seq_len(nrow(departements_L93))) {
   n <- as.numeric(departements_L93$CODE_DEPT[i])
   if (is.na(n)) {
     if (departements_L93$CODE_DEPT[i] == "2A") {
-      n = 96
+      n <- 96
     }
     else {
-      n = 97
+      n <- 97
     }
   }
   departements_L93$acc[i] <- depts[n]
   if (depts[n] != 0) {
-    departements_L93$taux_acc_grave[i] <- depts_grave[n] / depts[n]
+    departements_L93$taux_acc_grave[i] <- depts_grave[n] / depts[n] * 100
   }
   else {
     departements_L93$taux_acc_grave[i] <- 0
@@ -143,7 +145,7 @@ for (i in seq_len(nrow(departements_L93))) {
 for (i in seq_len(nrow(region_L93))) {
   region_L93$acc[i] <- reg[as.numeric(region_L93$CODE_REG[i])]
   if (reg[as.numeric(region_L93$CODE_REG[i])] != 0) {
-    region_L93$taux_acc_grave[i] <- reg_grave[as.numeric(region_L93$CODE_REG[i])] / reg[as.numeric(region_L93$CODE_REG[i])]
+    region_L93$taux_acc_grave[i] <- reg_grave[as.numeric(region_L93$CODE_REG[i])] / reg[as.numeric(region_L93$CODE_REG[i])] * 100
   }
   else {
     region_L93$taux_acc_grave[i] <- 0
@@ -211,6 +213,6 @@ print(g_region)
 dev.off()
 
 
-M <- as.table(rbind(c(762, 327, 468), c(484,239,477)))
-dimnames(M) <- list(gender=c("M","F"), party=c("Democrat","Independent", "Republican"))
+M <- 
+dimnames(M) <- list()
 (test <- chisq.test(M))
