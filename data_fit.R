26e11d597aaa63c00a79bafb07f1e7ce0f023f31
Binau02 <aubin.saunier@gmail.com>
2023-06-13 09:23:10 +0200

    Co-authored-by: Algolbarth <Algolbarth@users.noreply.github.com>

--- data_fit.R
+++ data_fit.R
@@ -1,3 +1,8 @@
+################
+# DATA fitting #
+################
+
+
 df <- read.csv("stat_acc_V3.csv", sep = ";")
 weights <- read.csv("poids_vehicules.csv", sep = ";")
 gravity <- read.csv("niveaux_gravite.csv", sep = ";")
@@ -28,6 +33,13 @@ df <- df[-test, ]
 df$age <- as.numeric(df$age)
 df$age <- df$age - 14
 
+
+
+######################
+# DATA visualisation #
+######################
+
+
 # plot by age
 hist(df$age, breaks = max(df$age)-min(df$age)+1, main = "number of accident by age", xlab = "age", ylab = "number of accident")
 
@@ -48,3 +60,28 @@ barplot(as.table(c(test["Indemne"], test["Blessé léger"], test["Blessé hospit
 test <- table(df["ville"])
 test <- test[order(test, decreasing = TRUE)]
 barplot(head(test, n = 30), las = 2)
+
+# r.install(tidyverse)
+library(tidyverse)
+# library(dplyr)
+# library(sf)
+# library(ggplot2)
+# library(tmap)
+# library(leaflet)
+# if (!file.exists(file.path(extraWD, "departement.zip"))) {
+#   githubURL <- "https://github.com/statnmap/blog_tips/raw/master/2018-07-14-introduction-to-mapping-with-sf-and-co/data/departement.zip"
+#   download.file(githubURL, file.path(extraWD, "departement.zip"))
+#   unzip(file.path(extraWD, "departement.zip"), exdir = extraWD)
+# }
+# departements_L93 <- st_read(dsn = extraWD, layer = "DEPARTEMENT",
+#                             quiet = TRUE) %>% 
+# st_transform(2154)
+# ggplot(departements_L93) +
+# aes(fill = CODE_REG) +
+# scale_fill_viridis_d() +
+# geom_sf() +
+# coord_sf(crs = 4326) +
+# guides(fill = FALSE) +
+# ggtitle("Coord. géographiques") +
+# theme(title = element_text(size = 16),
+#       plot.margin = unit(c(0,0.1,0,0.25), "inches"))
