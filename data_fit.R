63e81173e2f8ae8a2d90967c0d55d6711d13bb7e
Binau02 <aubin.saunier@gmail.com>
2023-06-13 13:59:26 +0200

    maps
    Co-authored-by: Algolbarth <Algolbarth@users.noreply.github.com>

--- data_fit.R
+++ data_fit.R
@@ -19,6 +19,7 @@ for (i in seq_len(nrow(gravity))) {
 for (i in seq_len(nrow(dept))) {
   df$dept[substr(df$id_code_insee, 1, 2) == dept[i, 1]] <- dept[i, 2]
   df$region[substr(df$id_code_insee, 1, 2) == dept[i, 1]] <- dept[i, 4]
+  df$CODE_REG[substr(df$id_code_insee, 1, 2) == dept[i, 1]] <- dept[i, 3]
 }
 
 test <- c()
@@ -56,32 +57,91 @@ test <- table(df$descr_grav)
 barplot(as.table(c(test["Indemne"], test["Blessé léger"], test["Blessé hospitalisé"], test["Tué"])), las = 2)
 
 # plot by city
-# print(table(df[c("id_code_insee", "ville")]))
 test <- table(df["ville"])
 test <- test[order(test, decreasing = TRUE)]
 barplot(head(test, n = 30), las = 2)
 
-# r.install(tidyverse)
-# library(tidyverse)
-# library(dplyr)
-# library(sf)
-# library(ggplot2)
-# library(tmap)
-# library(leaflet)
-# if (!file.exists(file.path(extraWD, "departement.zip"))) {
-#   githubURL <- "https://github.com/statnmap/blog_tips/raw/master/2018-07-14-introduction-to-mapping-with-sf-and-co/data/departement.zip"
-#   download.file(githubURL, file.path(extraWD, "departement.zip"))
-#   unzip(file.path(extraWD, "departement.zip"), exdir = extraWD)
-# }
-# departements_L93 <- st_read(dsn = extraWD, layer = "DEPARTEMENT",
-#                             quiet = TRUE) %>% 
-# st_transform(2154)
-# ggplot(departements_L93) +
-# aes(fill = CODE_REG) +
-# scale_fill_viridis_d() +
-# geom_sf() +
-# coord_sf(crs = 4326) +
-# guides(fill = FALSE) +
-# ggtitle("Coord. géographiques") +
-# theme(title = element_text(size = 16),
-#       plot.margin = unit(c(0,0.1,0,0.25), "inches"))
+#map
+extraWD <- "."
+library(tidyverse)
+library(dplyr)
+library(sf)
+library(ggplot2)
+library(tmap)
+library(leaflet)
+if (!file.exists(file.path(extraWD, "departement.zip"))) {
+  githubURL <- "https://github.com/statnmap/blog_tips/raw/master/2018-07-14-introduction-to-mapping-with-sf-and-co/data/departement.zip"
+  download.file(githubURL, file.path(extraWD, "departement.zip"))
+  unzip(file.path(extraWD, "departement.zip"), exdir = extraWD)
+}
+
+#use database
+departements_L93 <- st_read(dsn = extraWD, layer = "DEPARTEMENT", quiet = TRUE) %>% 
+st_transform(2154)
+
+#transform departements into regions
+region_L93 <- departements_L93 %>% 
+group_by(CODE_REG, NOM_REG) %>% 
+summarize()
+
+depts <- vector(mode = "numeric", length = 97)
+reg <- vector(mode = "numeric", length = 94)
+# print(depts)
+for (i in seq_len(nrow(df))) {
+  n <- as.numeric(substr(df$id_code_insee[i], 1, 2))
+  if (is.na(n)) {
+    if (substr(df$id_code_insee[i], 1, 2) == "2A") {
+      n = 96
+    }
+    else {
+      n = 97
+    }
+  }
+  if (n >= 1 && n <= 96) {
+    depts[n] <- depts[n] + 1
+  }
+  n <- as.numeric(df$CODE_REG[i])
+  reg[n] <- reg[n] + 1
+}
+for (i in seq_len(nrow(departements_L93))) {
+  n <- as.numeric(departements_L93$CODE_DEPT[i])
+  if (is.na(n)) {
+    if (departements_L93$CODE_DEPT[i] == "2A") {
+      n = 96
+    }
+    else {
+      n = 97
+    }
+  }
+  departements_L93$acc[i] <- depts[n]
+}
+for (i in seq_len(nrow(region_L93))) {
+  region_L93$acc[i] <- reg[as.numeric(region_L93$CODE_REG[i])]
+}
+# print(depts)
+
+#departements map
+g_departement <- ggplot(departements_L93) +
+  aes(color = acc) +
+  scale_color_continuous(low = "yellow", high = "red") +
+  geom_sf(aes(fill = acc)) +
+  # scale_fill_viridis_d() +
+  # scale_color_gradient(low = "green", high = "red") +
+  scale_fill_continuous(low = "yellow", high = "red") +
+  coord_sf(crs = 4326) +
+  guides(fill = FALSE) +
+  ggtitle("Départements") +
+  theme(title = element_text(size = 16), plot.margin = unit(c(0,0.1,0,0.25), "inches"))
+print(g_departement)
+
+#regions map
+g_region <- ggplot(region_L93) +
+  # aes(fill = CODE_REG) +
+  # scale_fill_viridis_d() +
+  geom_sf(aes(fill = acc)) +
+  scale_fill_continuous(low = "yellow", high = "red") +
+  coord_sf(crs = 2154, datum = sf::st_crs(2154)) +
+  guides(fill = FALSE) +
+  ggtitle("Régions") +
+  theme(title = element_text(size = 16))
+print(g_region)
