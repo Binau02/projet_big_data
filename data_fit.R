89e297555a2f9ddb983e4fe11824e2c76ae25a8a
maxime-code <maxime.roquelle@isen-ouest.yncrea.fr>
2023-06-14 09:35:20 +0200

    create csv dossier

--- data_fit.R
+++ data_fit.R
@@ -3,12 +3,12 @@
 ################
 
 
-df <- read.csv("stat_acc_V3.csv", sep = ";")
-weights <- read.csv("poids_vehicules.csv", sep = ";")
-gravity <- read.csv("niveaux_gravite.csv", sep = ";")
-climat <- read.csv("climat.csv", sep = ";")
-etat <- read.csv("etat.csv", sep = ";")
-dept <- read.csv("departements-france.csv")
+df <- read.csv("csv/stat_acc_V3.csv", sep = ";")
+weights <- read.csv("csv/poids_vehicules.csv", sep = ";")
+gravity <- read.csv("csv/niveaux_gravite.csv", sep = ";")
+climat <- read.csv("csv/climat.csv", sep = ";")
+etat <- read.csv("csv/etat.csv", sep = ";")
+dept <- read.csv("csv/departements-france.csv")
 
 for (i in seq_len(nrow(weights))) {
   df$weight[df$descr_cat_veh == weights[i, 1]] <- as.numeric(weights[i, 2])
